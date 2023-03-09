5a27aff32370351f6f93a0a747c2e067993b1080
Zhukov Vitaliy <felex180590@gmail.com>
2023-03-09 12:31:26 +0200

    adds redux-toolkit to phonebook project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { App } from 'components/App/App';
 
-import { persistor, store } from './redux/store';
+import { App } from 'components/App/App';
+import { store } from './redux/store';
+// import { persistor } from './redux/store';
 // import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
